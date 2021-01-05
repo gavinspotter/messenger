a881b11ec595bf75e31b1292e9bf515a02bb10a8
Osiris <potterosiris@gmail.com>
2021-01-05 14:45:15 -0500

    mongoose connect

--- server/app.js
+++ server/app.js
@@ -7,3 +7,14 @@ const app = express()
 app.use(bodyParser.json())
 
 
+
+mongoose
+    .connect(
+        "mongodb+srv://gavin:Password123@cluster0.d3tnt.mongodb.net/messenger?retryWrites=true&w=majority"
+    )
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
