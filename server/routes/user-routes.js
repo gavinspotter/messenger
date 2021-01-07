3fd65f0ce5a146994c9f4a6442dce827330a83f8
Osiris <potterosiris@gmail.com>
2021-01-06 20:06:44 -0500

    login

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -4,4 +4,6 @@ const userController = require("../controllers/user-controller")
 
 const router = express.Router()
 
-router.post("/email", userController)
+router.post("/signup", userController)
+
+router.post("/login", userController)
