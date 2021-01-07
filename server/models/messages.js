383798e26322f3f7d8ed4640ada7560140fa674c
Osiris <potterosiris@gmail.com>
2021-01-06 19:43:42 -0500

    message schema

--- server/models/messages.js
+++ server/models/messages.js
@@ -2,3 +2,8 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 
+const messageSchema = new Schema({
+    messsage: { type: String, required: true },
+    chat: [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }]
+})
+
