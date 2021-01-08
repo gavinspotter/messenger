7cf353cea6fbd63ac57f9690bd0151b86ea973bb
Osiris <potterosiris@gmail.com>
2021-01-08 13:37:12 -0500

    func

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,6 +1,10 @@
 const HttpError = require("../models/HttpError")
 const User = require("../models/user")
 
+const getUserByEmail = async (req, res, next) => {
+
+}
+
 const signup = async (req, res, next) => {
 
     const { name, email, password } = req.body
