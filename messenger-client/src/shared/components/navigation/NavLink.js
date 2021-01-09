2c1810acd0e2256e4feca07d684266dfeb332d01
Osiris <potterosiris@gmail.com>
2021-01-09 14:15:40 -0500

    use context

--- messenger-client/src/shared/components/navigation/NavLink.js
+++ messenger-client/src/shared/components/navigation/NavLink.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useContext } from "react"
 
 
 import { AuthContext } from "../../context/auth-context"
 import "./NavLink.css"
 
 const NavLinks = () => {
+    const auth = useContext(AuthContext)
 
 }
 
