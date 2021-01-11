f0002c7512eccd01336cbd61a538dec61e01d71a
Osiris <potterosiris@gmail.com>
2021-01-10 20:55:29 -0500

    mb schema

--- server/models/messageboards.js
+++ server/models/messageboards.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const messageboardSchema = new Schema({
+    messages: [{ type: mongoose.Types.ObjectId, required: true, ref: "Messages" }],
+    chat: [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }]
+})
+
--- server/models/messages.js
+++ server/models/messages.js
@@ -4,11 +4,7 @@ const Schema = mongoose.Schema
 
 const messageSchema = new Schema({
     message: { type: String, required: true },
-    chat:
-        [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }]
-
-
 })
 
-module.exports = mongoose.model("messages", messageSchema)
+module.exports = mongoose.model("Messages", messageSchema)
 
