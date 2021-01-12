cfed7bb268210df7c42f4de556f5038b2d5782a8
Osiris <potterosiris@gmail.com>
2021-01-11 19:36:21 -0500

    message schema

--- server/models/messages.js
+++ server/models/messages.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const messageSchema = new Schema({
     message: { type: String, required: true },
-    messageboard: { type: mongoose.Types.ObjectId, required: true, ref: "MessageBoard" }
+    messageboard: { type: mongoose.Types.ObjectId, required: true, ref: "MessageBoard" },
+    sender: { type: mongoose.Types.ObjectId, required: true, ref: "User" }
 })
 
 module.exports = mongoose.model("Messages", messageSchema)
