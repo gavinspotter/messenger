05872f201628efea93de73e01f76c133e4c92a34
Osiris <potterosiris@gmail.com>
2021-01-13 14:37:21 -0500

    props chat

--- messenger-client/src/messages/components/MbItem.js
+++ messenger-client/src/messages/components/MbItem.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Card from "../../shared/components/UIElements/Card"
 
-const MbItem = () => {
+const MbItem = (props) => {
 
     return (
         <li>
             <Card>
                 <div>
                     <div>
-
+                        {props.chat}
                     </div>
                 </div>
             </Card>
