1f6f07c1091f7be357d370e6c2442f04fc8174e7
Osiris <potterosiris@gmail.com>
2021-01-13 18:32:11 -0500

    import css

--- messenger-client/src/messages/components/MessageList.js
+++ messenger-client/src/messages/components/MessageList.js
@@ -1,7 +1,14 @@
 import React from "react"
 
+import "./MessageList.css"
+
 const MessageList = () => {
 
+    return (
+        <ul name="mb-list">
+
+        </ul>
+    )
 }
 
 export default MessageList
