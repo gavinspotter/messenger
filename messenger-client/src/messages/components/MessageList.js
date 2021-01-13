7c7eadeaf4ea847b9355b19e82309825d0010d65
Osiris <potterosiris@gmail.com>
2021-01-13 18:35:44 -0500

    map

--- messenger-client/src/messages/components/MessageList.js
+++ messenger-client/src/messages/components/MessageList.js
@@ -1,12 +1,20 @@
 import React from "react"
+import MessageItem from "./MessageItem"
 
 import "./MessageList.css"
 
-const MessageList = () => {
+const MessageList = (props) => {
 
     return (
         <ul name="mb-list">
-
+            {
+                props.items.map((stuff) =>
+                    <MessageItem
+                        key={stuff._id}
+                        messages={stuff.messages}
+                    />
+                )
+            }
         </ul>
     )
 }
