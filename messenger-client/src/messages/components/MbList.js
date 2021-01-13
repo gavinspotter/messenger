561c454cccc5918d88dde55bf95de8c4bf1cb7a1
Osiris <potterosiris@gmail.com>
2021-01-13 13:02:25 -0500

    mbl

--- messenger-client/src/messages/components/MbList.js
+++ messenger-client/src/messages/components/MbList.js
@@ -1,7 +1,15 @@
 import React from "react"
+import MbItem from "./MbItem"
 
 const MbList = () => {
 
+    return (
+        <ul className="mb-items">
+
+
+            <MbItem />
+        </ul>
+    )
 }
 
 export default MbList
