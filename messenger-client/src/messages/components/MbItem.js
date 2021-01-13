394e0c3aa7d81b44bd6979890772d77edf494c70
Osiris <potterosiris@gmail.com>
2021-01-13 16:58:22 -0500

    state

--- messenger-client/src/messages/components/MbItem.js
+++ messenger-client/src/messages/components/MbItem.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Card from "../../shared/components/UIElements/Card"
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
@@ -7,21 +7,26 @@ import "./MbItem.css"
 
 const MbItem = (props) => {
 
+    const [chat1, setChat1] = useState()
+    const [chat2, setChat2] = useState()
+
+
     const { isLoading, error, sendRequest, clearError } = useHttpClient()
 
     useEffect(() => {
         const fetchEmail = async () => {
             try {
-                const responseData = (
+                const responseData = await sendRequest(
                     `http://localhost:5000/api/user/findemail/${props.chat[0]}`
                 )
-
+                setChat1(responseData.user.email)
             } catch (err) {
 
             }
         }
+        fetchEmail()
 
-    })
+    }, [sendRequest, props.chat])
 
 
 
@@ -31,7 +36,7 @@ const MbItem = (props) => {
             <Card>
                 <div>
                     <div>
-                        {props.chat[0]}<br />
+                        {chat1}<br />
                         {props.chat[1]}<br />
                         {props.chat[2]}
 
