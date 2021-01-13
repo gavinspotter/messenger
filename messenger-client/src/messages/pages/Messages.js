9e1362f87e1cd4e30d80c2d2fddbd29ee690bfd7
Osiris <potterosiris@gmail.com>
2021-01-13 18:19:41 -0500

    params

--- messenger-client/src/messages/pages/Messages.js
+++ messenger-client/src/messages/pages/Messages.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { useParams } from "react-router-dom"
 
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
@@ -8,6 +9,8 @@ const Messages = () => {
     const [loadedMessageBoard, setLoadedMessageBoard] = useState()
 
     const { isLoading, error, sendRequest, clearError } = useHttpClient()
+
+    const mbId = useParams().mbId
 
     return (
         <div>
