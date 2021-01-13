c58623f09ddcac44ae19e522e0057ad005d5acc4
Osiris <potterosiris@gmail.com>
2021-01-13 16:52:01 -0500

    use effect

--- messenger-client/src/messages/components/MbItem.js
+++ messenger-client/src/messages/components/MbItem.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Card from "../../shared/components/UIElements/Card"
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
@@ -8,6 +8,21 @@ import "./MbItem.css"
 const MbItem = (props) => {
 
     const { isLoading, error, sendRequest, clearError } = useHttpClient()
+
+    useEffect(() => {
+        const fetchEmail = async () => {
+            try {
+                const responseData = (
+                    `http://localhost:5000/api/user/findemail/${props.chat[0]}`
+                )
+
+            } catch (err) {
+
+            }
+        }
+
+    })
+
 
 
 
