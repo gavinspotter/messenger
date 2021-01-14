a25da507117b9e29d3de5c7d2857a7a8dd4b77ed
Osiris <potterosiris@gmail.com>
2021-01-14 15:05:02 -0500

    use state

--- messenger-client/src/messages/components/MessageItem.js
+++ messenger-client/src/messages/components/MessageItem.js
@@ -1,11 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import Card from "../../shared/components/UIElements/Card"
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
 
 const MessageItem = (props) => {
 
+    const [message, setMessage] = useState()
+
     const { isLoading, error, sendRequest, clearError } = useHttpClient()
+
+
 
 
 
