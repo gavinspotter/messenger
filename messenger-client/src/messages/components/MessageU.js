3460c4298c526b55d6b9d3ccb5394c41c5e12713
Osiris <potterosiris@gmail.com>
2021-01-14 16:14:48 -0500

    http hook

--- messenger-client/src/messages/components/MessageU.js
+++ messenger-client/src/messages/components/MessageU.js
@@ -1,3 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 
-const MessageU = () => { }
+import { useHttpClient } from "../../shared/hooks/http-hook"
+
+const MessageU = () => {
+
+
+    useEffect(() => {
+        const fetchMessages = async () => {
+            try {
+                const responseData = await sendRequest(
+                    `http://localhost:5000/api/messages/getmessages/${props.messages}`
+                )
+                setMessage(responseData.message.message)
+            } catch (err) {
+
+            }
+
+        }
+        fetchMessages()
+    }, [sendRequest, props.messages])
+
+
+
+}
+
+export default MessageU
