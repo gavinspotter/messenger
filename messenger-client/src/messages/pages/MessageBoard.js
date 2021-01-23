9010ed69fcc55373828a64ada4398e794ea01d62
Osiris <potterosiris@gmail.com>
2021-01-23 18:09:26 -0500

    context

--- messenger-client/src/messages/pages/MessageBoard.js
+++ messenger-client/src/messages/pages/MessageBoard.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
 import { useParams } from "react-router-dom"
+import Button from "../../shared/components/FormElements/Button"
 import Input from "../../shared/components/FormElements/Input"
 import Card from "../../shared/components/UIElements/Card"
 import ErrorModal from "../../shared/components/UIElements/ErrorModal"
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner"
 
-
+import { AuthContext } from "../../shared/context/auth-context"
 import { useHttpClient } from "../../shared/hooks/http-hook"
 import MbList from "../components/MbList"
 
@@ -24,7 +25,80 @@ const MessageBoard = () => {
     const userId = useParams().userId
 
     const onSubmit = async (data) => {
+        if (data.player1 === "") {
+            try {
+                await sendRequest(
+                    `http://localhost:5000/api/messages/createmb`,
+                    "POST",
+                    JSON.stringify({
+                        chat: [
+                            data.player2,
+                            data.player3
+                        ]
+                    }),
+                    {
+                        "Content-Type": "application/json"
+                    }
+                )
+            } catch (err) {
 
+            }
+        } else if (data.player2 === "") {
+            try {
+                await sendRequest(
+                    `http://localhost:5000/api/messages/createmb`,
+                    "POST",
+                    JSON.stringify({
+                        chat: [
+                            data.player1,
+                            data.player3
+                        ]
+                    }),
+                    {
+                        "Content-Type": "application/json"
+                    }
+                )
+            } catch (err) {
+
+            }
+        } else if (data.player3 === "") {
+            try {
+                await sendRequest(
+                    `http://localhost:5000/api/messages/createmb`,
+                    "POST",
+                    JSON.stringify({
+                        chat: [
+                            data.player1,
+                            data.player2
+                        ]
+                    }),
+                    {
+                        "Content-Type": "application/json"
+                    }
+                )
+            } catch (err) {
+
+            }
+        } else {
+            try {
+                await sendRequest(
+                    `http://localhost:5000/api/messages/createmb`,
+                    "POST",
+                    JSON.stringify({
+                        chat: [
+                            data.player1,
+                            data.player2,
+                            data.player3
+                        ]
+                    }),
+                    {
+                        "Content-Type": "application/json"
+                    }
+                )
+            } catch (err) {
+
+            }
+        }
 
 
     }
@@ -64,7 +138,7 @@ const MessageBoard = () => {
                 </div>
             )}
             <Card>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <Input
                         element="input"
                         valRef={register}
@@ -73,13 +147,10 @@ const MessageBoard = () => {
                     <Input
                         element="input"
                         valRef={register}
-                        name="player1"
+                        name="player2"
                     />
-                    <Input
-                        element="input"
-                        valRef={register}
-                        name="player1"
-                    />
+
+                    <Button>start messenging</Button>
                 </form>
             </Card>
             {!isLoading && loadedMb && <MbList items={loadedMb} />}
