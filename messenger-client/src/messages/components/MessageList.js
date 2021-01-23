25c6c981bf028e34f26b688d7ddf3ca5a85f31bd
Osiris <potterosiris@gmail.com>
2021-01-23 16:34:38 -0500

    reverse map

--- messenger-client/src/messages/components/MessageList.js
+++ messenger-client/src/messages/components/MessageList.js
@@ -13,7 +13,7 @@ const MessageList = (props) => {
                         key={data}
                         messages={data}
                     />
-                )
+                ).reverse()
             }
         </ul>
     )
