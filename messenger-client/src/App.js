5a638d1d180d827db0ee55f9bdefae98c9e7a6db
Osiris <potterosiris@gmail.com>
2021-01-25 13:31:10 -0500

    login

--- messenger-client/src/App.js
+++ messenger-client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react"
+import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import MessageBoard from "./messages/pages/MessageBoard"
 import Messages from "./messages/pages/Messages"
