148fe987321ad890199d4ca4bc0cf805ed7ad808
Osiris <potterosiris@gmail.com>
2021-01-25 13:16:26 -0500

    tc token

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -153,6 +153,20 @@ const login = async (req, res, next) => {
 
     let token;
 
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError(
+            'Logging in failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
+
     res.json({
         message: "logged in",
         user: existingUser.toObject({ getters: true })
